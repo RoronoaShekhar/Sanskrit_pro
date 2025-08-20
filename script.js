3d9e0117fc2ebfe115ce5533a4d38778a41fc4e0
RoronoaShekhar <nineynine.ninenine@gmail.com>
2025-08-20 16:38:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+pkg update && pkg upgrade -y
+pkg install python ffmpeg -y
+pip install yt-dlp
+
+nano ~/.bashrc
+
+H() {
+    if [ -z "$1" ]; then
+        echo "Usage: H <YouTube_URL>"
+        return 1
+    fi
+    yt-dlp -f "bestvideo+bestaudio/best" --merge-output-format mp4 "$1"
+}
+
+
+nano ~/.zshrc
+
+
+source ~/.bashrc
+
 window.addEventListener('DOMContentLoaded', () => {
   const fadeEls = document.querySelectorAll('.fade-in');
 
